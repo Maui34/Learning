491e893a697943ebd07bd6a5e861d430bfa9baf7
Maui34 <rafal.pietrzak50@wp.pl>
2022-08-09 21:21:09 +0200

    Create Pierwszy.java

---
+++ Pierwszy/src/pierwszy/Pierwszy.java
@@ -0,0 +1,32 @@
+
+package pierwszy;
+
+
+public class Pierwszy {
+
+   
+    
+    public static void main(String[] args) {
+        int a = 103;
+        int b = 56;
+        int c = 123;
+        int d = 33;
+        String numberA = a % 2 == 0 ? "even" : "odd";
+            System.out.println(numberA);
+            
+        String numberB = b % 2 == 0 ? "even" : "odd";
+            System.out.println(numberB);
+        
+        String numberC = c % 2 == 0 ? "even" : "odd";
+            System.out.println(numberC);
+        
+        String numberD = d % 2 == 0 ? "even" : "odd";
+            System.out.println(numberD);  
+        
+        
+        
+        
+ 
+    }
+    
+}
